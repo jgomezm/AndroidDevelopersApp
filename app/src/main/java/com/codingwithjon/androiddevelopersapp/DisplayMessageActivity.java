db9f241a247a1b1ca232840a768a847b95665848
jonathan <jonathanm800@gmail.com>
2016-05-18 19:27:03 -0400

    completed commit

---
+++ app/src/main/java/com/codingwithjon/androiddevelopersapp/DisplayMessageActivity.java
@@ -0,0 +1,23 @@
+package com.codingwithjon.androiddevelopersapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setText(message);
+        textView.setTextSize(40);
+        RelativeLayout Layout = (RelativeLayout) findViewById(R.id.content);
+        Layout.addView(textView);
+    }
+}
